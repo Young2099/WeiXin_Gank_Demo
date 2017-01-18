9a3728d2fd6e1655cf291bf9ff22b07c6a085f9b
yangfang <15868178345@163.com>
2017-01-18 10:42:22 +0800

    加载更多实现成功

--- pages/homes/home.js
+++ pages/homes/home.js
@@ -3,10 +3,8 @@ var app = getApp();
 var Constant = require('../../utils/constants.js');
 Page({
   data:{
-    images:[{
-        url:"../../images/mm.jpg"}],
+    images:[],
   },
-      lastId:0,
   onLoad:function(){
      var that = this;
     // 页面初始化 options为页面跳转所带来的参数
@@ -15,6 +13,7 @@ Page({
   },
   loadMore:function(event){
     var that =this; 
+    // 这里是为了替换获取数据的page数目
     var index =Constant.GET_URL.lastIndexOf("\/");
     var length=  Constant.GET_URL.lenght;
     var str  = Constant.GET_URL .substring(index + 1,length);
@@ -30,12 +29,15 @@ function requestData(that,targetPage,str){
             },
        success: function(res) {
             console.log(res.data.results.length);
-             console.log(res.data.results);
-          that.setData({
-            images:res.data.results
-             });
-              mCurrentPage = targetPage;
-              console.log(mCurrentPage);
+            console.log(res.data.results);
+            var dataArr = that.data.images
+            //合并数据，使加载更多成功
+            var newData = dataArr.concat(res.data.results)
+            that.setData({
+              images: newData
+                });
+                mCurrentPage = targetPage;
+                console.log(mCurrentPage);
         }
     })
 }
